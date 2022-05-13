23c7e371e9f81d40d3d729dcd280327a8c2d9b1e
AcilRestu12 <arieskarestu214@gmail.com>
2022-05-13 22:27:22 +0700

    Redesign front-end of navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,13 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/bootstrap.css" rel="stylesheet">
 
     {{-- Bootstrap Icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     
     {{-- My Style --}}
     <link rel="stylesheet" href="/css/style.css">
+
+    {{-- Font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Seaweed+Script&display=swap" rel="stylesheet">
     
     <title>Rest Blog | {{ $title }}</title>
   </head>
@@ -21,9 +26,12 @@
     {{-- Memanggil file navbar.blade.php di folder partials --}}
     @include('partials.navbar')
     
-    <div class="container mt-4">
+    <div class="container mt-5">
         @yield('container')                {{-- Akan berisi apapun yg ada di halaman-halaman child-nya yg memiliki section container--}}
     </div>
+
+
+    @include('partials.footer')
       
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,51 @@
+<div class="container" style=".b-example-divider {
+    height: 3rem;
+    background-color: rgba(0, 0, 0, .1);
+    border: solid rgba(0, 0, 0, .15);
+    border-width: 1px 0;
+    box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+  }
+  
+  .bi {
+    vertical-align: -.125em;
+    fill: currentColor;
+  }">
+    <footer class="row row-cols-5 py-5 my-5 border-top">
+      <div class="col">
+        <a class="navbar-brand" href="/" style="font-family: 'Seaweed Script', cursive;; 
+        font-style: normal; 
+        font-weight: 400;
+        font-size: 4.5em;
+        line-height: 98px; 
+        color: #303030; 
+        text-shadow: 0px 2px 3px rgba(0, 0, 0, 0.15);">
+            Rest Blog
+        </a>
+      </div>
+
+      <div class="col">
+          
+      </div>
+    
+      <div class="col ms-auto">
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+        </ul>
+      </div>
+  
+      <div class="col">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+        </ul>
+      </div>
+    </footer>
+</div>
